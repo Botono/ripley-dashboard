9be3c61e42e8964f842664f7f1f8506b12e03a4f
Aaron Murray <aaron@aaron-murray.net>
2019-02-03 16:15:37 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Nav from './components/Nav';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Nav />
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-sm-3">
+              <div class="chart-wrapper">
+                <div class="chart-title">
+                  Side Bar Title
+                </div>
+                <div class="chart-stage">
+                  <img data-src="holder.js/100%x650/white" />
+                </div>
+                <div class="chart-notes">
+                  Notes about this chart
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-9">
+              <div class="row">
+                <div class="col-sm-12">
+                  <div class="chart-wrapper">
+                    <div class="chart-title">
+                      Activity
+                    </div>
+                    <div class="chart-stage">
+                      <img data-src="holder.js/100%x240/white" />
+                    </div>
+                    <div class="chart-notes">
+                      Notes about this chart
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-sm-12">
+                  <div class="chart-wrapper">
+                    <div class="chart-title">
+                      Water
+                    </div>
+                    <div class="chart-stage">
+                      <img data-src="holder.js/100%x240/white" />
+                    </div>
+                    <div class="chart-notes">
+                      Notes about this chart
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+
     );
   }
 }
