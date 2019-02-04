3a11c21605120a2437d7b7ce2e860ca1862a1139
Aaron Murray <aaron@aaron-murray.net>
2019-02-04 15:47:59 -0800

    changes

--- src/common/config.js
+++ src/common/config.js
@@ -1,5 +1,5 @@
 const Config = {};
 
-Config.api_url = 'https://0fjc71o959.execute-api.us-west-1.amazonaws.com/v1'
+Config.api_url = 'https://4nhxysbd2c.execute-api.us-west-2.amazonaws.com/v1'
 
 export default Config;
